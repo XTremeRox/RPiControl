dfab762e70d85ced87d2326c84214c2c6f3e6d20
xtremerox <ramatheracer@gmail.com>
2019-09-20 14:33:05 +0530

    added average analog

--- server.py
+++ server.py
@@ -6,18 +6,23 @@
 from flask import Flask, request , jsonify, render_template
 import RPi.GPIO as GPIO
 import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in as AnalogIn
 #Production server settings 
 #from gevent.pywsgi import WSGIServer
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(16,GPIO.OUT)
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(10,GPIO.OUT)
-GPIO.setup(4,GPIO.OUT)
-GPIO.setup(9,GPIO.OUT)
-GPIO.setup(27,GPIO.OUT)
-GPIO.setup(22,GPIO.OUT)
-GPIO.setup(17,GPIO.OUT)
+GPIO.setup(16,GPIO.OUT) #test_led
+GPIO.setup(11,GPIO.OUT) #main_switch
+GPIO.setup(10,GPIO.OUT) #volt_inc_mode
+GPIO.setup(4,GPIO.OUT) #memory
+GPIO.setup(9,GPIO.OUT) #ht_start
+GPIO.setup(27,GPIO.OUT) #inc_volt
+GPIO.setup(22,GPIO.OUT) #dec_volt
+GPIO.setup(17,GPIO.OUT) #ht_stop
+GPIO.setup(5,GPIO.OUT) #memory_relay
 app = Flask(__name__, template_folder = 'html_code')
 #Password
 secretpasskey = '1234'
@@ -45,12 +50,22 @@ def status():
 	return jsonify(response)
 @app.route('/reading', methods=['GET'])
 def reading():
-	#TODO - Get analog reading value from 
-	return jsonify({'reading':'22.5'})
+	#taking 30 reading for average
+	i2c = busio.I2C(board.SCL, board.SDA)
+	ads = ADS.ADS1115(i2c)
+	channel = AnalogIn(ads, ADS.P3)
+	sum = 0.00
+	for i in range(0,30):
+		sum+=channel.value
+	avg = str('%.4f'%(sum/30))
+	return jsonify({'reading':avg})
 @app.route('/push', methods=['POST'])
 def push():
-	#TODO - Get pushrequest and activate for 50ms and set state to 0
-	pass
+	#Get pushrequest and activate for 30 reading and set state to 0 and return reading
+	GPIO.output(5, GPIO.HIGH) #memory_relay high
+	read = reading()
+	GPIO.output(5, GPIO.LOW) #memory_relay high
+	return read
 @app.route('/toggle', methods=['POST'])
 def toggle():
 	#change pin state and authentication using POST
